54dd2f220f36fcf603e260867cda477efe9bc899
IncognitoJohn <155202956+IncognitoJohn@users.noreply.github.com>
2026-05-05 19:06:34 +0100

    I did something well

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,60 @@ import UserMessage from "./UserMessage.jsx";
 import BotMessage from "./BotMessage.jsx";
 
  const App= ()=> {
-     const Messages=[
+     const UserMessages=[
 
          {
-             text:"hello chatbot"
+             text:"hello chatbot",
+            key:"id_1"
 
          },
          {
-             text:"Hello! How can I help you?"
+             text:"Can you get me today's date?",
+            key:"id_2"
+
+         }
+     ];
+     const BotMessages=[
+
+
+         {
+             text:"Hello! How can I help you?",
+             key:"id_1"
 
          },
          {
-             text:"Can you get me today's date?"
+             text:"Today's date is 4th May",
+             key:"id_2"
 
-         },
-         {
-             text:"Today's date is 4th May"
-
-         },
+         }
 
      ];
 
-  return (
+const chatMessageComponent=UserMessages.map((mess,ID)=>{
+    return(< UserMessage text = {mess.text}
+                         key={mess.key}
+                         />
+
+    )
+})
+     const BotComponent=BotMessages.map((mess,ID)=>{
+         return(< BotMessage text = {mess.text}
+                              key={mess.key}
+             />
+
+         )
+     })
+
+
+
+
+     return (
       <div>
         <ChatInput/>
         <div className="mt-4 ml-4  flex flex-col">
 
-          <UserMessage text="hello chatbot" />
-          <BotMessage text="Hello! How can I help you?" />
-            <UserMessage text="Can you get me today's date?"/>
-            <BotMessage text="Today's date is 4th May" />
-
+            {chatMessageComponent}
+            {BotComponent}
 
         </div>
       </div>
